e78476711a228a508d06536d3e0198caec861000
Your Name <sidban222@gmail.com>
2023-01-23 18:06:38 +0530

    working code

--- assignment1/Assignment1.asm
+++ assignment1/Assignment1.asm
@@ -4,6 +4,14 @@ global main
 default rel  ; use this by default for efficiency. This is even mandatory if you run your code on macOS.
 
 
+%macro scan_int 1
+mov rdi, Input_format ;loading format
+mov rsi, %1
+call scanf
+
+%endmacro
+
+
 
 section .data
 
@@ -24,24 +32,17 @@ section .text
 main:
 ; scan n and k from stdin
 push rbp ; align stack
-mov rdi, Input_format ;loading format
-mov rsi, n 
-call scanf
-mov rdi, Input_format ;loading format
-mov rsi, k
-call scanf
+scan_int n
+scan_int k
 pop rbp  ;restores stack
 
-; push rbp ;realign stack
 
 ; scan n integers from stdin
 movsx rbx, dword [n]
 mov r12, arr
 push rbp
 scan_loop:
-mov rdi, Input_format
-mov rsi, r12
-call scanf
+scan_int r12
 dec rbx
 add r12, 4
 cmp rbx, 0x0
@@ -64,7 +65,6 @@ adder:
 ; ; add the integers and print result
 mov rax, 0
 mov r12, 0
-
 add_loop:
 movsx rdx, dword[arr+ 4*r12]
 add rax, rdx
@@ -73,6 +73,7 @@ cmp r12d, [k]
 jne add_loop
 
 
+; print result
 mov rdi, output_fmt
 mov rsi, rax
 mov al, 0
@@ -83,83 +84,4 @@ call printf
 ; ; pop rbp
 ; ;simulates return 0
 movsx rax, dword [e_code]
-; mov rax, 0
 ret
-
-
-;  functions
-strlen:
-    push rbx
-    mov rbx, rax
-
-.nextchar:
-    cmp  byte [rax], 0
-    jz .finished
-    inc rax
-    jmp .nextchar
-
-.finished:
-    sub rax, rbx
-    pop rbx
-    ret
-; end of strlen function
-
-; print_str prints the string starting from the memory location stored in eax and ending at the next null character
-print_str:
-    push rbx
-    push rcx
-    push rdx
-    push rax
-
-    mov rcx, rax
-    call strlen
-    mov rdx, rax
-    mov rax, 4
-    mov rbx, 1
-    int 0x80
-    pop rax
-    pop rdx
-    pop rcx
-    pop rbx
-    ret
-; end of print_str function
-
-
-print_rax:
-    push rax
-    mov [char_buf], al
-    mov rax, char_buf
-    call print_str
-    pop rax
-    ret
-
-
-
-; print_int:
-;     push rax
-;     push rdx
-;     push rcx
-;     push rsi
-;     mov rcx, 0
-; .div_loop:
-;     mov rsi, 10
-;     mov rdx, 0
-;     div rsi
-;     inc rcx
-;     add rdx, '0'
-;     push rdx
-;     cmp rax, 0
-;     jnz .div_loop
-
-; .print_loop:
-;     pop rax
-;     call print_rax
-;     dec rcx
-;     cmp rcx, 0
-;     jnz .print_loop
-
-;     pop rsi
-;     pop rcx
-;     pop rdx
-;     pop rax
-;     ret    
